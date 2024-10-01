286acb60c517b7f890226148ce4c516abd248b00
MarlonHuynh <Bettytuduyen@gmail.com>
2024-10-01 02:27:07 -0700

    card sprites added and ui test

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -6,24 +6,43 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private OrthographicCamera camera;
+    private FitViewport viewport;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        image = new Texture("cardsprites/balancedbinarytree.png");
+
+        // Set up camera and viewport
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(600, 600, camera); // 600x600 is the virtual world size
+        viewport.apply();
+
+        // Center the camera
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        camera.update();
+
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, viewport.getWorldWidth()/4, viewport.getWorldHeight()/4, viewport.getWorldWidth()/2, viewport.getWorldHeight()/2);
         batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 
     @Override
