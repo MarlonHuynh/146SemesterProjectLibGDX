e5c9c0742159846ebd4abe0f029c783269d70087
MarlonHuynh <Bettytuduyen@gmail.com>
2024-10-26 02:35:28 -0700

    added highlight for selected cards.

--- core/src/main/java/io/github/dataevolutionclasses/CardOnScreenData.java
+++ core/src/main/java/io/github/dataevolutionclasses/CardOnScreenData.java
@@ -29,39 +29,133 @@ public class CardOnScreenData {
     private Card card;
     private Sprite cardSprite;
     private Sprite cardbackSprite;
+    private Sprite selectedSprite;
     private float x, y;
     private float scale;
+    //
+    private BitmapFont nameFont;
+    private float nameX;
+    private float nameY;
+    private float descWidth;
+    private GlyphLayout descLayout;
+    private float descX;
+    private float descY;
+    private BitmapFont numberFont;
+    private float costTextX;
+    private float costTextY;
+    private float attackTextX;
+    private float attackTextY;
+    private float shieldTextX;
+    private float shieldTextY;
 
     public CardOnScreenData(Card card, float x, float y, float scale){
         this.card = card;
         this.cardSprite = new Sprite(card.getTexture());
         this.cardbackSprite = new Sprite(new Texture("cardback2.png"));
+        if (card.getName().equals("Draw")){
+            this.cardbackSprite = new Sprite(new Texture("cardback3.png"));
+        }
+        else if (card.getName().equals("Trash")){
+            this.cardbackSprite = new Sprite(new Texture("cardback4.png"));
+        }
+        this.selectedSprite = new Sprite(new Texture("CardbackHighlight.png"));
         this.x = x;
         this.y = y;
         this.scale = scale;
-        System.out.println("COSD: " + Float.toString(x) + ", " + Float.toString(y) + ", Scale: " + Float.toString(scale));
-    }
-    public Sprite getSprite(){
-        return cardSprite;
-    }
-    public Card getCard(){
-        return card;
-    }
-    public float getX(){
-        return x;
-    }
-    public float getY(){
-        return y;
-    }
-    public float getScale(){
-        return scale;
-    }
-    public Sprite getBackSprite(){
-        return cardbackSprite;
-    }
-    public void setSprite(Sprite newSprite){
-        cardSprite = newSprite;
-    }
+        // Set card sizing and position (doesn't directly draw)
+        selectedSprite.setSize(selectedSprite.getTexture().getWidth() * scale, selectedSprite.getTexture().getHeight() * scale);
+        cardbackSprite.setSize(cardbackSprite.getTexture().getWidth() * scale, cardbackSprite.getTexture().getHeight() * scale);
+        cardSprite.setSize(cardSprite.getTexture().getWidth() * scale, cardSprite.getTexture().getHeight() * scale);
+        float cardX = (x - ((cardbackSprite.getWidth() * cardbackSprite.getScaleX()) / 2)); // Bottom left corner x
+        float cardY = (y - ((cardbackSprite.getHeight() * cardbackSprite.getScaleY()) / 2)); // Bottom left corner y
+        float midcardX = ((cardbackSprite.getWidth() * cardbackSprite.getScaleX()) / 2);  // Distance from the card's left edge to middle
+        float midcardY = ((cardbackSprite.getHeight() * cardbackSprite.getScaleY()) / 2); // Distance from the card's bottom edge to middle
+        cardbackSprite.setPosition(cardX, cardY);
+        selectedSprite.setPosition(cardX - (midcardX * 0.1f), cardY - (midcardY * 0.06f));
+        cardSprite.setPosition(cardX + (midcardX / 3.5f), cardY + (midcardY / 1.3f));
 
+        // Set text sizing and position (doesn't directly draw)
+        nameFont = new BitmapFont(Gdx.files.internal("ui/dpcomic.fnt"));
+        nameFont.getData().setScale(scale * 0.75f);
+        nameX =  cardX + (midcardX * 0.16f);
+        nameY = cardY + (midcardY * 1.88f);
+        descWidth = 130 * scale;
+        descLayout = new GlyphLayout();
+        descLayout.setText(nameFont, card.getDesc(), Color.BLACK, 130 * scale, Align.left, true);
+        descX = cardX + (midcardX * 0.16f);
+        descY = cardY + (midcardY * 0.54f);
+        numberFont = new BitmapFont(Gdx.files.internal("ui/dpcomic.fnt"));
+        numberFont.getData().setScale(scale * 0.9f);
+        costTextX = cardX + (midcardX * 0.2f);
+        costTextY =  cardY + (midcardY * 1.62f);
+        attackTextX = cardX + (midcardX * 1.64f);
+        attackTextY =  cardY + (midcardY * 0.56f);
+        shieldTextX = cardX + (midcardX * 1.64f);
+        shieldTextY =  cardY + (midcardY * 0.22f);
+        // Debug
+        // System.out.println("COSD: " + Float.toString(x) + ", " + Float.toString(y) + ", Scale: " + Float.toString(scale));
+    }
+    // Getters and Setters
+    public Card getCard() { return card; }
+    public void setCard(Card card) { this.card = card; }
 
+    public Sprite getCardSprite() { return cardSprite; }
+    public void setCardSprite(Sprite cardSprite) { this.cardSprite = cardSprite; }
+
+    public Sprite getSelectedSprite() { return selectedSprite; }
+    public void setSelectedSprite(Sprite selectedSprite) {this.selectedSprite = selectedSprite; }
+
+    public Sprite getCardbackSprite() { return cardbackSprite; }
+    public void setCardbackSprite(Sprite cardbackSprite) { this.cardbackSprite = cardbackSprite; }
+
+    public float getX() { return x; }
+    public void setX(float x) { this.x = x; }
+
+    public float getY() { return y; }
+    public void setY(float y) { this.y = y; }
+
+    public float getScale() { return scale; }
+    public void setScale(float scale) { this.scale = scale; }
+
+    public BitmapFont getNameFont() { return nameFont; }
+    public void setNameFont(BitmapFont nameFont) { this.nameFont = nameFont; }
+
+    public float getNameX() { return nameX; }
+    public void setNameX(float nameX) { this.nameX = nameX; }
+
+    public float getNameY() { return nameY; }
+    public void setNameY(float nameY) { this.nameY = nameY; }
+
+    public float getDescWidth() { return descWidth; }
+    public void setDescWidth(float descWidth) { this.descWidth = descWidth; }
+
+    public GlyphLayout getDescLayout() { return descLayout; }
+    public void setDescLayout(GlyphLayout descLayout) { this.descLayout = descLayout; }
+
+    public float getDescX() { return descX; }
+    public void setDescX(float descX) { this.descX = descX; }
+
+    public float getDescY() { return descY; }
+    public void setDescY(float descY) { this.descY = descY; }
+
+    public BitmapFont getNumberFont() { return numberFont; }
+    public void setNumberFont(BitmapFont numberFont) { this.numberFont = numberFont; }
+
+    public float getCostTextX() { return costTextX; }
+    public void setCostTextX(float costTextX) { this.costTextX = costTextX; }
+
+    public float getCostTextY() { return costTextY; }
+    public void setCostTextY(float costTextY) { this.costTextY = costTextY; }
+
+    public float getAttackTextX() { return attackTextX; }
+    public void setAttackTextX(float attackTextX) { this.attackTextX = attackTextX; }
+
+    public float getAttackTextY() { return attackTextY; }
+    public void setAttackTextY(float attackTextY) { this.attackTextY = attackTextY; }
+
+    public float getShieldTextX() { return shieldTextX; }
+    public void setShieldTextX(float shieldTextX) { this.shieldTextX = shieldTextX; }
+
+    public float getShieldTextY() { return shieldTextY; }
+    public void setShieldTextY(float shieldTextY) { this.shieldTextY = shieldTextY; }
 }
--- core/src/main/java/io/github/dataevolutionclasses/Main.java
+++ core/src/main/java/io/github/dataevolutionclasses/Main.java
@@ -44,6 +44,10 @@ public class Main extends ApplicationAdapter {
     private ArrayList<CardOnScreenData> cardOnScreenDatas;  // Houses all information about all cards displayed on the screen
     private ArrayList<String> CardInstancesNames;           // Named instances of the card for easy debugging
     private int selectedCardNumber = -1;            // Changes when a player clicks on a card (starts at -1 for no card selected)
+    GlyphLayout descLayout = new GlyphLayout();
+    float descWidth;
+
+    private BitmapFont font;
 
 
     // Instantiated upon startup
@@ -61,33 +65,36 @@ public class Main extends ApplicationAdapter {
         // Initialize startup Font
         defaultFont = new BitmapFont(Gdx.files.internal("ui/dpcomic.fnt"));
         defaultFont.getData().setScale(0.6f);
+        // Initialize card font
+        font = new BitmapFont(Gdx.files.internal("ui/dpcomic.fnt"));
         // Set up array of Sprite names and sprites to keep track of the sprites on screen for input handling
         cardOnScreenDatas = new ArrayList<CardOnScreenData>();
         CardInstancesNames = new ArrayList<String>();
         // Create all cards on screen
-        // Enemy's hand (Instance 1-5)
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(0), viewport.getWorldWidth() * (2 / 16f), viewport.getWorldHeight() * (14 / 16f), 0.4f));
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(1), viewport.getWorldWidth() * (4.5f / 16f), viewport.getWorldHeight() * (14 / 16f), 0.4f));
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(2), viewport.getWorldWidth() * (7 / 16f), viewport.getWorldHeight() * (14 / 16f), 0.4f));
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(3), viewport.getWorldWidth() * (9.5f / 16f), viewport.getWorldHeight() * (14 / 16f), 0.4f));
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(4), viewport.getWorldWidth() * (12 / 16f), viewport.getWorldHeight() * (14 / 16f), 0.4f));
-        //  Player's hand (Instance 6-10)
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(5), viewport.getWorldWidth() * (2 / 16f), viewport.getWorldHeight() * (2 / 16f), 0.4f));
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(6), viewport.getWorldWidth() * (4.5f / 16f), viewport.getWorldHeight() * (2 / 16f), 0.4f));
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(7), viewport.getWorldWidth() * (7 / 16f), viewport.getWorldHeight() * (2 / 16f), 0.4f));
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(8), viewport.getWorldWidth() * (9.5f / 16f), viewport.getWorldHeight() * (2 / 16f), 0.4f));
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(9), viewport.getWorldWidth() * (12 / 16f), viewport.getWorldHeight() * (2 / 16f), 0.4f));
-        // Field top (enemy) (Instance 11-13)
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(10), viewport.getWorldWidth() * (4.5f / 16f), viewport.getWorldHeight() * (10 / 16f), 0.5f));
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(11), viewport.getWorldWidth() * (7 / 16f), viewport.getWorldHeight() * (10 / 16f), 0.5f));
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(12), viewport.getWorldWidth() * (9.5f / 16f), viewport.getWorldHeight() * (10 / 16f), 0.5f));
-        // Field bottom (player) (Instance 15-16)
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(13), viewport.getWorldWidth() * (4.5f / 16f), viewport.getWorldHeight() * (6 / 16f), 0.5f));
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(14), viewport.getWorldWidth() * (7 / 16f), viewport.getWorldHeight() * (6 / 16f), 0.5f));
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(15), viewport.getWorldWidth() * (9.5f / 16f), viewport.getWorldHeight() * (6 / 16f), 0.5f));
-        // Deck draw (Instance 17)
-        // TODO: To be replaced with cardList.get(number) of the card that just looks like a deck, no monster, just a pile of cards.
-        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(35), viewport.getWorldWidth() * (14 / 16f), viewport.getWorldHeight() * (6 / 16f), 0.5f));
+        // Enemy's hand (Instance 0-4)
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(0), viewport.getWorldWidth() * (2 / 16f), viewport.getWorldHeight() * (14 / 16f), 0.45f));
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(1), viewport.getWorldWidth() * (4.5f / 16f), viewport.getWorldHeight() * (14 / 16f), 0.45f));
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(2), viewport.getWorldWidth() * (7 / 16f), viewport.getWorldHeight() * (14 / 16f), 0.45f));
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(3), viewport.getWorldWidth() * (9.5f / 16f), viewport.getWorldHeight() * (14 / 16f), 0.45f));
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(4), viewport.getWorldWidth() * (12 / 16f), viewport.getWorldHeight() * (14 / 16f), 0.45f));
+        //  Player's hand (Instance 5-9)
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(5), viewport.getWorldWidth() * (2 / 16f), viewport.getWorldHeight() * (2 / 16f), 0.45f));
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(6), viewport.getWorldWidth() * (4.5f / 16f), viewport.getWorldHeight() * (2 / 16f), 0.45f));
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(7), viewport.getWorldWidth() * (7 / 16f), viewport.getWorldHeight() * (2 / 16f), 0.45f));
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(8), viewport.getWorldWidth() * (9.5f / 16f), viewport.getWorldHeight() * (2 / 16f), 0.45f));
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(9), viewport.getWorldWidth() * (12 / 16f), viewport.getWorldHeight() * (2 / 16f), 0.45f));
+        // Field top (enemy) (Instance 10-12)
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(10), viewport.getWorldWidth() * (4.2f / 16f), viewport.getWorldHeight() * (10.1f / 16f), 0.5f));
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(11), viewport.getWorldWidth() * (7 / 16f), viewport.getWorldHeight() * (10.1f / 16f), 0.5f));
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(12), viewport.getWorldWidth() * (9.8f / 16f), viewport.getWorldHeight() * (10.1f / 16f), 0.5f));
+        // Field bottom (player) (Instance 14-15)
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(13), viewport.getWorldWidth() * (4.2f / 16f), viewport.getWorldHeight() * (5.9f / 16f), 0.5f));
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(14), viewport.getWorldWidth() * (7 / 16f), viewport.getWorldHeight() * (5.9f / 16f), 0.5f));
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(15), viewport.getWorldWidth() * (9.8f / 16f), viewport.getWorldHeight() * (5.9f / 16f), 0.5f));
+        // Deck draw (Instance 16)
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(35), viewport.getWorldWidth() * (14.7f / 16f), viewport.getWorldHeight() * (6.2f / 16f), 0.5f));
+        // Trash (Instance 17)
+        cardOnScreenDatas.add(new CardOnScreenData(cardList.get(36), viewport.getWorldWidth() * (14.7f / 16f), viewport.getWorldHeight() * (2 / 16f), 0.5f));
         for (int i = 0; i < cardOnScreenDatas.size(); i ++){
             CardInstancesNames.add("Card " + cardOnScreenDatas.get(i).getCard().getName() + ", Instance #" + Integer.toString(i + 1));
             System.out.println("spriteName added: " + CardInstancesNames.get(i));
@@ -118,8 +125,9 @@ public class Main extends ApplicationAdapter {
         Vector3 worldCoords = camera.unproject(new Vector3(screenX, screenY, 0));
         // Loop through each sprite and check if the click is within its bounding rectangle
         for (int i = 0; i < cardOnScreenDatas.size(); i++) {
-            if (cardOnScreenDatas.get(i).getSprite().getBoundingRectangle().contains(worldCoords.x, worldCoords.y)) {
-                selectedCardNumber = i + 1;
+            if (cardOnScreenDatas.get(i).getCardSprite().getBoundingRectangle().contains(worldCoords.x, worldCoords.y)
+                || cardOnScreenDatas.get(i).getCardbackSprite().getBoundingRectangle().contains(worldCoords.x, worldCoords.y)) {
+                selectedCardNumber = i;
                 return CardInstancesNames.get(i); // Return the name of the clicked sprite
             }
         }
@@ -148,20 +156,24 @@ public class Main extends ApplicationAdapter {
     public void draw(){
         // Clears screen and prepares batch for drawing
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        // Draw Select Sprite if needed
+        if (selectedCardNumber != -1){
+            drawSelected(cardOnScreenDatas.get(selectedCardNumber), camera);
+        }
         // Test drawing card from drawCard function
         for (int i = 0; i < cardOnScreenDatas.size(); i++) {
             drawCard(cardOnScreenDatas.get(i), camera);
         }
+        // Display FPS counter and position of cursor
         int screenX =  Gdx.input.getX();
         int screenY =  Gdx.input.getY();
         Vector3 worldCoords = camera.unproject(new Vector3(screenX, screenY, 0));
-        // Display FPS counter and position of cursor
         SpriteBatch fpsBatch = new SpriteBatch();
         fpsBatch.setProjectionMatrix(camera.combined);
         fpsBatch.begin();
-        defaultFont.draw(fpsBatch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 10, 20); // Display FPS in bottom-left corner
-        defaultFont.draw(fpsBatch, "X: " + screenX, 10, 60);
-        defaultFont.draw(fpsBatch, "Y: " + screenY, 10, 45);
+        defaultFont.draw(fpsBatch, "FPS: " + Gdx.graphics.getFramesPerSecond(), 530, 540); // Display FPS in bottom-left corner
+        defaultFont.draw(fpsBatch, "X: " + worldCoords.x, 530, 580);
+        defaultFont.draw(fpsBatch, "Y: " + worldCoords.y, 530, 560);
         fpsBatch.end();
 
     }
@@ -170,61 +182,38 @@ public class Main extends ApplicationAdapter {
         if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
             if (!isLeftButtonPressed) {     // isLeftButtonPressed starts out False
                 isLeftButtonPressed = true; // This + assigning it true right after makes it so only one instance of a left-click is detected until the left click button is released.
-                // Console logs for debugging purposes
-                int x = Gdx.input.getX();
-                int y = Gdx.input.getY();
-                //System.out.println("Left mouse button clicked at (" + x + ", " + y + ")");
-                //System.out.println(cardListIndex);
             }
         } else {
             // Reset the flag when the button is released to prevent multiple left clicks being detected per frame
             isLeftButtonPressed = false;
         }
     }
-
-    public void drawCard(CardOnScreenData cardOnScreenData, OrthographicCamera camera){
-        Card card = cardOnScreenData.getCard();
-        float x = cardOnScreenData.getX();
-        float y = cardOnScreenData.getY();
-        float scale = cardOnScreenData.getScale();
-        // Initial card back image
-        Texture cardbackTexture = new Texture("cardback2.png");
-        Sprite cardbackSprite = cardOnScreenData.getBackSprite();
-        cardbackSprite.setSize(cardbackTexture.getWidth() * scale, cardbackTexture.getHeight() * scale);
-        // Initial card creature image
-        Texture cardCreatureTexture = card.getTexture();
-        Sprite cardCreatureSprite = cardOnScreenData.getSprite();
-        cardCreatureSprite.setSize(cardCreatureSprite.getTexture().getWidth() * scale, cardCreatureSprite.getTexture().getHeight() * scale);
-        // Set Font
-        BitmapFont font = new BitmapFont(Gdx.files.internal("ui/dpcomic.fnt"));
-        font.setColor(Color.BLACK);
-        font.getData().setScale(scale * 0.7f);
+    public void drawSelected(CardOnScreenData CoSD, OrthographicCamera camera) {
+        SpriteBatch batch = new SpriteBatch();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        CoSD.getSelectedSprite().draw(batch);
+        batch.end();
+        camera.update();
+    }
+    public void drawCard(CardOnScreenData CoSD, OrthographicCamera camera){
+        Card card = CoSD.getCard();
         // Create batch of sprite to be drawn
         SpriteBatch batch = new SpriteBatch();
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        // Find position of card
-        // Note: DO NOT USE PIXELS to get your position. Use relative sizing.
-        float cardX = (x - ((cardbackSprite.getWidth() * cardbackSprite.getScaleX()) / 2)); // Bottom left corner x
-        float cardY = (y - ((cardbackSprite.getHeight() * cardbackSprite.getScaleY()) / 2)); // Bottom left corner y
-        float midcardX = ((cardbackSprite.getWidth() * cardbackSprite.getScaleX()) / 2);  // Distance from the card's left edge to middle
-        float midcardY = ((cardbackSprite.getHeight() * cardbackSprite.getScaleY()) / 2); // Distance from the card's bottom edge to middle
-        // ----- Images -----
-        cardbackSprite.setPosition(cardX, cardY);
-        cardbackSprite.draw(batch); // Draw card back
-        cardCreatureSprite.setPosition(cardX+(midcardX/3.5f), cardY+(midcardY/1.5f));
-        cardCreatureSprite.draw(batch); // Draw card creature
-        // ----- Text -----
-        font.draw(batch, card.getName(), cardX+(midcardX*0.6f), cardY+(midcardY*1.82f)); // Draw name text
-        GlyphLayout descLayout = new GlyphLayout();
-        float descWidth = 120 * scale;
-        font.getData().setScale(scale * 0.6f);
-        descLayout.setText(font, card.getDesc(), Color.BLACK, descWidth, Align.left, true);
-        font.draw(batch, descLayout, cardX+(midcardX*0.25f), cardY+(midcardY*0.5f)); // Draw description
-        font.getData().setScale(scale * 0.8f);
-        font.draw(batch, Integer.toString(card.getCost()), cardX+(midcardX*0.25f), cardY+(midcardY*1.82f)); // Draw cost text
-        font.draw(batch, Integer.toString(card.getAttack()), cardX+(midcardX*1.6f), cardY+(midcardY*0.5f)); // Draw attack text
-        font.draw(batch, Integer.toString(card.getShield()), cardX+(midcardX*1.6f), cardY+(midcardY*0.25f)); // Draw shield text
+        // Draw cardback
+        CoSD.getCardbackSprite().draw(batch);
+        if (!card.getName().equals("Draw") && !card.getName().equals("Trash")) {
+            // Draw creature
+            CoSD.getCardSprite().draw(batch);
+            // Draw text
+            CoSD.getNameFont().draw(batch, card.getName(), CoSD.getNameX(), CoSD.getNameY());
+            CoSD.getNameFont().draw(batch, CoSD.getDescLayout(), CoSD.getDescX(), CoSD.getDescY());
+            CoSD.getNumberFont().draw(batch, Integer.toString(card.getCost()), CoSD.getCostTextX(), CoSD.getCostTextY());
+            CoSD.getNumberFont().draw(batch, Integer.toString(card.getAttack()), CoSD.getAttackTextX(), CoSD.getAttackTextY());
+            CoSD.getNumberFont().draw(batch, Integer.toString(card.getShield()), CoSD.getShieldTextX(), CoSD.getShieldTextY());
+        }
         // End batch and update camera frame
         batch.end();
         camera.update();
