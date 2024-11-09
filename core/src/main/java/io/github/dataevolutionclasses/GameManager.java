5fdf88abc187382a22ddc006e43e901e55b9f71b
AnanranG <jiaqiao.han@sjsu.edu>
2024-11-09 15:51:37 -0800

    Update GameManager.java

--- core/src/main/java/io/github/dataevolutionclasses/GameManager.java
+++ core/src/main/java/io/github/dataevolutionclasses/GameManager.java
@@ -1,10 +1,28 @@
 package io.github.dataevolutionclasses;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GameManager extends Game {
+public class GameManager extends Game implements ApplicationListener {
+    public SpriteBatch batch;
+    public BitmapFont font;
+
     @Override
     public void create() {
-        setScreen(new Title(this)); // Set initial screen to Title
+        batch = new SpriteBatch();
+        font = new BitmapFont(); // use default Arial font
+        this.setScreen(new Title(this)); // Set initial screen to Title
     }
+
+    public void render() {
+        super.render(); // important!!!
+    }
+
+    public void dispose(){
+        batch.dispose();
+        font.dispose();
+    }
+
 }
