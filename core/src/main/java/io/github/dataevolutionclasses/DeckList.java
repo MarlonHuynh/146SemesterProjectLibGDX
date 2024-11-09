665771c576d2a879b468d9de919fcfa9afad255c
“Dom” <dominic.dang@sjsu.edu>
2024-11-09 10:06:38 -0800

    Recent updates to Decklist and TestDeck

--- core/src/main/java/io/github/dataevolutionclasses/DeckList.java
+++ core/src/main/java/io/github/dataevolutionclasses/DeckList.java
@@ -1,7 +1,15 @@
 package io.github.dataevolutionclasses;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,18 +22,56 @@ public class DeckList extends ApplicationAdapter
     public static List<Card> cardList;
     private final HashMap<String, Card> nameToCardHashmap = new HashMap<>();            // Master Card Storage (Do not change)
     private final HashMap<String, Integer> nameToIntHashmap = new HashMap<>();
+    private SpriteBatch spriteBatch;
+    private Stage stage;
+    private Scroll scroll;
+    private OrthographicCamera camera;              // Camera
+    private FitViewport viewport;                   // Viewport
+    private Sprite cardbackSprite;                  // Sprite for card back
+    private Sprite cardCreatureSprite;              // Sprite for card creature
+    private BitmapFont font;                        // Font
+    private float cardScale = 1f;                   // Card scale (affects all the other text placements according to the card size)
 
+    public void draw()
+    {
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        scroll = new Scroll();
+        scroll.createSlider(stage); //Add the slider into the stage
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(600, 600, camera);                   // 600x600 is the virtual world size
+        viewport.apply();
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0); // Center the camera
+        // Read and generate cards and place cards into Card list
+        CardReader reader = new CardReader("core/src/main/java/io/github/dataevolutionclasses/TestDeck.csv");
+        reader.generateCardsFromCSV();
+        cardList = reader.getCardList();
+        // Initial startup card back image
+        Texture cardbackTexture = new Texture("cardback2.png");
+        cardbackSprite = new Sprite(cardbackTexture);
+        // Initial startup card creature image
+        Texture cardCreatureTexture = new Texture("hashmap.png");
+        cardCreatureSprite = new Sprite(cardCreatureTexture);
+        // Initialize startup Font
+        font = new BitmapFont(Gdx.files.internal("ui/dpcomic.fnt"));
+        font.getData().setScale(cardScale * 0.5f);
+    }
+
+    @Override
     public void create()
     {
+        // Initialize drawBatch
+        spriteBatch = new SpriteBatch();
 
-        CardReader reader = new CardReader("core/src/main/java/io/github/dataevolutionclasses/CardStats2.csv");
+        CardReader reader = new CardReader("core/src/main/java/io/github/dataevolutionclasses/TestDeck.csv");
         reader.generateCardsFromCSV();
         cardList = reader.getCardList();
 
         // Static initializer block to populate initial cards
         // Create multiple decks
         List<Card> decks = new ArrayList<>();
-
 
         // Create first deck
         List<String> deck1 = Arrays.asList(
@@ -42,12 +88,6 @@ public class DeckList extends ApplicationAdapter
         for (String str : deck1){
             deck.add(nameToCardHashmap.get(str).deepCopy());
         }
-
-
-
-
-
-
     }
 
 }
