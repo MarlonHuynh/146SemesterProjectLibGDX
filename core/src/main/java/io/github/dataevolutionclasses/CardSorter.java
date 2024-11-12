e5b2ff30a0749e68273169f0d2963d7e6eeee8a2
irynavlasiuk <i.vlasiu.04@gmail.com>
2024-11-12 09:58:36 -0800

    Sort class fix

--- core/src/main/java/io/github/dataevolutionclasses/CardSorter.java
+++ core/src/main/java/io/github/dataevolutionclasses/CardSorter.java
@@ -5,34 +5,6 @@ import java.util.Comparator;
 import java.util.List;
 
 public class CardSorter {
-//    public static List<Card> sortByName(List<Card> cards) {
-//        return cards.stream().sorted(Comparator.comparing(Card::getName)).collect(Collectors.toList());
-////        cards.stream().map(e->e.getName()).collect(Collectors.toList()).forEach(System.out::println);
-////        Collections.sort(cards, Comparator.comparing(Card::getName));
-////        cards.stream().map(e->e.getName()).collect(Collectors.toList()).forEach(System.out::println);
-//    }
-//
-//    public static List<Card> sortByStage(List<Card> cards) {
-////        Collections.sort(cards, Comparator.comparingInt(Card::getStage));
-//        return cards.stream().sorted(Comparator.comparingInt(Card::getStage)).collect(Collectors.toList());
-//    }
-//
-//    public static List<Card> sortByCost(List<Card> cards) {
-////        Collections.sort(cards, Comparator.comparingInt(Card::getCost));
-//        return cards.stream().sorted(Comparator.comparingInt(Card::getCost)).collect(Collectors.toList());
-//    }
-//
-//    public static List<Card> sortByAttack(List<Card> cards) {
-////        Collections.sort(cards, Comparator.comparingInt(Card::getAttack));
-//        return cards.stream().sorted(Comparator.comparingInt(Card::getAttack)).collect(Collectors.toList());
-//    }
-//
-//    public static List<Card> sortByShield(List<Card> cards) {
-////        Collections.sort(cards, Comparator.comparingInt(Card::getShield));
-//        return cards.stream().sorted(Comparator.comparingInt(Card::getShield)).collect(Collectors.toList());
-//    }
-
-
     public static List<Card> sortByName(List<Card> cards) {
         return mergeSort(new ArrayList<>(cards), Comparator.comparing(Card::getName));
     }
